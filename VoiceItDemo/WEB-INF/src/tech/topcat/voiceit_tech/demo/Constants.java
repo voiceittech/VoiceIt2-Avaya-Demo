0797749930d602383cd1d67303207f705d7d7432
Top Cat Technology Solutions <support@topcat-tech.com>
2018-11-09 10:21:10 -0700

    Initial checkin of API 2.0 application.

---
+++ VoiceItDemo/WEB-INF/src/tech/topcat/voiceit_tech/demo/Constants.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Top Cat Technology Solutions, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Top Cat Technology Solutions, LLC. - initial API and implementation
+ ******************************************************************************/
+package tech.topcat.voiceit_tech.demo;
+
+public final class Constants {
+	public static final String PROPERTY_FILE = "users.properties";
+}
